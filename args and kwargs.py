d39eded3b148582ec56d094a000520a8357d51b7
Azhar uddin sheikh <73925826+AzharUddinSheikh@users.noreply.github.com>
2020-11-10 12:51:33 +0530

    Add files via upload

--- args and kwargs.py
+++ args and kwargs.py
@@ -7,21 +7,20 @@ def function(name,age,rollno):
     print("the name is",name,"and age is",age,"whose roll no is",rollno)
 
 
-# function('azhar',26,'12')
+function('azhar',26,'12')
     
     
-
 def function1(*args):
     print("the name is",args[0],"and age is",args[1],"whose roll no is",args[2])
 
 
-# function1('sheikh',23,11)
+function1('sheikh',23,11)
 
 list1 = ['azhar',23,11]
 
 # function(list1) # Error agrs[0] will takes whole list1 as its valeue
 
-# function(*list1)
+function(*list1)
 
 '''*arg takes no of variable
 value of sheikh ,23, and 11 stores in *agrs and 
@@ -30,7 +29,6 @@ we didnt provide no of parameter in function1()
 
 # function1('sheikh',23) # error you provide args[2] and not provided it
   
-
 #if someone provided only 2 parameter 
 
 def function2(*args):
@@ -40,7 +38,7 @@ def function2(*args):
         print("the name is",args[0],"and age is",args[1])
         
 
-# function2('ajju',27)
+function2('ajju',27)
 
 # **kwargs
 '''.items() is dict function its provide pair key values'''
@@ -48,10 +46,9 @@ def function2(*args):
 def printmark(**kwargs):
     for key,i in kwargs.items():
         print(key,i)
-    
-    
+        
 marklist = {'azhar':33,'uddin':23,'sheikh':44,'ajju':45,'valar':80,'honey':44}    
-# printmark(**marklist)
+printmark(**marklist)
 
 '''benefit lets update marklist by .update or insert method or just 
 write directly and feed one more key and value (honey and 44 respectively 
@@ -71,4 +68,4 @@ def master(normal,*arg,**kwargs):
 list1 = ['azhar',23,44]
 marklist = {'azhar':33,'uddin':23,'sheikh':44,'ajju':45,'valar':80,'honey':44}
 
-# master("normal", *list1, **marklist)
+master("normal", *list1, **marklist)
